eeaf5cf15cd98d61ddc87ccb1ba176956a8a7365
Fynn <enriquefynn@gmail.com>
2017-07-05 16:29:11 +0100

    our bet-tracker

---
+++ ebets.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.4.11;
+
+import './bet.sol';
+
+contract ebets {
+  address resolver;
+
+  event created_bet(address bet_addr);
+  function ebets() {
+    resolver = msg.sender;
+  }
+  
+  function create_bet(string title, string category, 
+                      string team_0, string team_1, uint timestamp_match_begin,
+                      uint timestamp_match_end, uint timestamp_hard_deadline,
+                      uint timestamp_terminate_deadline, string url_oraclize) {
+    address bet = new Bet(this, title, category, team_0, team_1, 
+                              timestamp_match_begin, timestamp_match_end,
+                              timestamp_hard_deadline, timestamp_terminate_deadline,
+                              url_oraclize);
+    created_bet(bet);
+  }
+}
