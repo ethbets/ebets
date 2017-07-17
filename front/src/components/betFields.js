87faae5aed3cd003e8c03c09b9253d4aaa4d528b
Fynn <enriquefynn@gmail.com>
2017-07-17 13:45:54 +0200

    betFields

---
+++ front/src/components/betFields.js
@@ -0,0 +1,18 @@
+var BETFIELDS = {
+  bet_state: '',
+  is_featured: false,
+  title: '',
+  category: '',
+  team_0: '',
+  team_1: '',
+  team_0_bet_sum: 0,
+  team_1_bet_sum: 0,
+  bets_to_team_0: {},
+  bets_to_team_1: {},
+  timestamp_match_begin: 0,
+  timestamp_match_end: 0,
+  timestamp_hard_deadline: 0,
+  timestamp_terminate_deadline: 0,
+  url_oraclize: '',
+};
+export default BETFIELDS;
