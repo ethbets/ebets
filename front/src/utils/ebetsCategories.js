7003659e46debd1ab998b44fe9e2ff526e4139be
Fynn <enriquefynn@gmail.com>
2017-08-01 08:53:45 +0200

    categories

---
+++ front/src/utils/ebetsCategories.js
@@ -0,0 +1,20 @@
+const ebetsCategories = [
+  {
+    name: 'UFC',
+    path: 'ufc'
+  },
+  {
+    name: 'League of Legends',
+    path: 'esports/lol'
+  },
+  {
+    name: 'CS-GO',
+    path: 'esports/csgo'
+  },
+  {
+    name: 'Football',
+    path: 'football'
+  }
+]
+
+export default ebetsCategories;
