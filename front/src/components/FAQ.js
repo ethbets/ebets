69a79bdeb1b84ac69f507bbbe835cc388b90926b
Leonardo Alt <leonardoaltt@gmail.com>
2017-08-30 19:59:26 +0200

    FAQ improvements

--- front/src/components/FAQ.js
+++ front/src/components/FAQ.js
@@ -6,6 +6,7 @@
 */
 
 import React, { Component } from 'react';
+import { Card, CardHeader, CardText } from 'material-ui/Card';
 import HowDoesItWork from '../assets/imgs/Making-a-bet.png';
 
 class FAQ extends Component {
@@ -15,90 +16,124 @@ class FAQ extends Component {
 
   render() {
     return (
-      <div style={{marginLeft: 50}}>
-        <h4>You can find more information on <a href='https://github.com/ethbets/ebets'>github</a></h4>
-        <ul>
-          <li>How does it work?</li>
-          <p>
-            Users create bets including but not limited to "sports".
-            Users can bet on created bets, after a pre determined time
-            an arbiter can be summoned to cast the decision, this arbiter can be
-            an oracle, or any smart contract. After the match is decided users
-            can withdraw their prizes or cry their loss.
-          </p>
-          <li>What are featured bets? What's the difference?</li>
-          <p>
-            Featured bets are bets that we, the owners do, or think are good enough to appear
-            in the front pages. But anyone can create bets in our system, they will go to the
-            unfeatured tab, because we cannot guarantee the integrity of the arbiter you choose.
-          </p>
-          <li>What are arbiters?</li>
-          <p>
-            This is where the critical part of our contracts reside, so make sure you
-            understand what they are!
-          </p>
-          <p>
-            We plan to create a set of governance smart contracts,
-            so any one can become an arbiter by implementing our interface: <a 
-              href='https://github.com/ethbets/ebets/blob/master/contracts/governanceInterface.sol'>
-            governanceInterface.sol</a>.
-            We aim for every comunity implementing a smart contract for their own sports,
-            when there is a conflict, there is going to be a higher instance that can arbitrate,
-            much likely how the actual judiciary systems work nowadays.
-          </p>
-          <p>
-            By doing this, you can create your own private bets without loss of generality,
-            for instance, if Alice and Bob want to do a competition, and they choose Charlie,
-            to arbitrate the outcome and act like an escrow among them.
-            Alice, Bob and Charlie, are going to witness the competition, and decide
-            on the outcome together. The contract can be, for example, one that can only decide on the outcome
-            when a simple majority is reached. If Alice and Bob are both honests, the match is decided without the
-            intervention of Charlie.
-          </p>
+      <div style={{marginLeft: 80, marginRight: 80}}>
+        <Card style={{marginBottom: 20}}>
+          <CardHeader
+            title="What is this?"
+            actAsExpander={true}
+            showExpandableButton={true}
+          />
+          <CardText expandable={true}>
+            Ebets is an Ethereum based betting platform.
+            The bets are implemented as smart contracts, and you can check them out on our <a href='https://github.com/ethbets/ebets/tree/master/contracts'>GitHub</a>.
+          </CardText>
+        </Card>
 
-          <li>Are you a prediction market?</li>
-          <p>Not necessarily</p>
-          <li>Do you have a colored-paper?</li>
-          <p>
-            We have a bunch of published papers if you want! They neither qualify nor disqualify
-            the Dapp, you can read the Dapp's code in github.
-          </p>
-          <li>Why are you doing this?</li>
-          <p>We want bets, and we want them now!
-            No ICO, no bullshit! This is a set of well writen smart contracts
-            that perform simple tasks, make sports bets accessible.
-          </p>
-          <li>What are the fees?</li>
-          <p>
-            1% of the loosing side, this will be always transparent to the user.
-            One can also create bets with any fee, even 0.
-          </p>
-          <li>How do you guarantee not to steal my money?</li>
-          <p>
-            The only attack we can do is bet in a team
-            and lie in the arbiter part (if we control it), saying that the wrong team won.
-            We plan to outsource and build a comunity of arbiters for
-            the each bet category to overcome this issue.
-          </p>
-          <li>How do I play?</li>
-          <p>
-            You need to connect to ethereum with your favorite client and execute the contracts with the
-            web interface.
-          </p>
-          <li>What if I don't withdraw when I win?</li>
-          <p>
-            If the arbiter doesn't arbitrate in time, the match is said to be drawn,
-            and the users can withdraw what they bet. After a period, the contract can
-            be self-destructed and we collect the rest as fees.
-          </p>
-          <li>Do you plan on going in the live network?</li>
-          <p>Yes</p>
-          <li>Why your website is ugly?</li>
-          <p>It works, doesn't it?</p>
-          <li>Where are the contracts?</li>
-          <p>In <a href='https://github.com/ethbets/ebets/blob/master/contracts/'>github</a></p>
-        </ul>
-        {/* <img style={{flex: 1, resizeMode: 'contain'}} src={HowDoesItWork} /> */}
+        <Card style={{marginBottom: 20}}>
+          <CardHeader
+            title="How does it work?"
+            actAsExpander={true}
+            showExpandableButton={true}
+          />
+          <CardText expandable={true}>
+            Users may put their money on existing bets or create their own bets. The bets are usually related
+            to sports or e-sports, but not necessarily. When the match ends, an arbiter is invoked to decide
+            the outcome. The winners are then allowed to collect their rewards.
+          </CardText>
+        </Card>
+
+        <Card style={{marginBottom: 20}}>
+          <CardHeader
+            title="How do I bet?"
+            actAsExpander={true}
+            showExpandableButton={true}
+          />
+          <CardText expandable={true}>
+            Choose a team to bet on, the amount of money you want to bet, and press Bet!
+            By default, you are betting in Ether. You can also bet ERC20 tokens by changing the
+            currency field to one of the common tokens, or typing the Ethereum address of the token.
+            Once you bet on a team, regardless the currency, you are not allowed to bet on the other team.
+          </CardText>
+        </Card>
+
+        <Card style={{marginBottom: 20}}>
+          <CardHeader
+            title="I want to create a bet. What is an arbiter?"
+            actAsExpander={true}
+            showExpandableButton={true}
+          />
+          <CardText expandable={true}>
+            The arbiter is the entity that decides the outcome of the bet it is assigned to.
+            As the arbiter is seen as an Ethereum address, it may be a person, a group of people,
+            a smart contract, an oracle, or many other things. Currently we use the Monarch arbiter
+            for our bets, but you can of course use any arbiter of your choice when creating a bet.
+          </CardText>
+        </Card>
+
+        <Card style={{marginBottom: 20}}>
+          <CardHeader
+            title="What are featured/unfeatured bets?"
+            actAsExpander={true}
+            showExpandableButton={true}
+          />
+          <CardText expandable={true}>
+            Featured bets are the ones created by the Ebets team. Bets created by users are marked as unfeatured,
+            and not shown by default. You can click on Unfeatured on the left menu to enable all the bets to be visible.
+            Please keep in mind that we cannot guarantee the quality of the arbiters chosen by users.
+          </CardText>
+        </Card>
+
+        <Card style={{marginBottom: 20}}>
+          <CardHeader
+            title="Are there any fees?"
+            actAsExpander={true}
+            showExpandableButton={true}
+          />
+          <CardText expandable={true}>
+            Ebets keeps 1% of the losing pool.
+          </CardText>
+        </Card>
+
+        <Card style={{marginBottom: 20}}>
+          <CardHeader
+            title="How do I know you are not stealing my money?"
+            actAsExpander={true}
+            showExpandableButton={true}
+          />
+          <CardText expandable={true}>
+            Almost the entire system is controlled by our open source smart contracts,
+            available on our <a href='https://github.com/ethbets/ebets/tree/master/contracts'>GitHub</a>.
+            The only possible attack against your money, in case a bet uses our Monarchy arbiter,
+            is for us to lie about the outcome of a match. For now this is entirely reputation based,
+            since one wrong result from us will lead to the fall of Ebets, which we do not want to happen.
+            We plan to outsource and build a community of arbiters related to each bet category
+            (similar to a justice system, if you will) to improve our system.
+          </CardText>
+        </Card>
+
+         <Card style={{marginBottom: 20}}>
+          <CardHeader
+            title="What if the arbiter doesn't decide on any result?"
+            actAsExpander={true}
+            showExpandableButton={true}
+          />
+          <CardText expandable={true}>
+            The arbiter has a deadline to make a decision. If the decision is not made within this time limit,
+            the match is declared a draw and every participant gets their money back.
+          </CardText>
+        </Card>
+
+        <Card style={{marginBottom: 20}}>
+          <CardHeader
+            title="What if I win a bet but don't collect my rewards?"
+            actAsExpander={true}
+            showExpandableButton={true}
+          />
+          <CardText expandable={true}>
+            If you win a bet you will have quite a lot of time to collect your rewards (several days).
+            If you don't, the bet will be self-destructed and the remaining money will be collected as fees. 
+          </CardText>
+        </Card>
       </div>
     );
   }
