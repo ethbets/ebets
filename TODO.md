cfafef37b3446e570b6eee5d9f2ab363c0261b1a
Leonardo Alt <leonardoaltt@gmail.com>
2017-09-30 10:30:21 +0200

    Tab title contains proper name. Updated TODO.

--- TODO.md
+++ TODO.md
@@ -1,35 +1,7 @@
-Branch erc20 TODO list:
-
-Layout:
-- [X] When there are many bets in the list, the pool numbers look somewhat confusing (the blue chips).
-      Suggestions:
-      1) Use a fixed size for the blue chips and teams names
-      2) Use a slightly different background color for odd/even bets in the list
-- [X] Improve withdraw list layout
-- [ ] Improve WIN layout
-- [X] Improve FAQ
-- [X] Add new categories
-- [X] Add icons of new categories
-- [X] Add ERC20 tokens
-
-Logic:
-- [X] Withdraw: Send list of tokens to withdraw, like this the user can pay the price
-  of doing only the necessary bet withdraws.
-- [X] Use ERC20 decimals. For now default is 18
-- [X] If the user bet on the loser team but no one bet on the winner, user gets their money back
-
 Front
-- [X] Withdraw: compute withdraw value according to the selected currency
-- [X] Activate "loading bar" already when Approval request is sent
-- [X] Dialog asking for confirmation before Approve
-- [X] Dialog asking for confirmation before Bet (after Approve)
-- [X] Dialog listing all the tokens that the user will Withdraw when pressed
-      "Withdraw" button
+- [ ] Improve category icons
 
 Bugs
 - [ ] When creating a bet: Uncaught (in promise) Error: Invalid number of arguments to Solidity function
-- [X] Category icon not showing
-- [X] When Approval doesn't work, the promise resolves anyway and the current JS code thinks it's time to send the real Bet transaction. This should be fixed.
-- [X] When the page is loaded without an account, the user is able to choose the team in a bet that they've already bet when they log into their metamask account
-      (reload page when account changes)
 - [ ] Permalink: category icon not shown
+- [ ] Bunch of warnings 'Request cancelled!'
--- front/src/components/Master.js
+++ front/src/components/Master.js
@@ -16,6 +16,8 @@ import withWidth, {MEDIUM, LARGE} from 'material-ui/utils/withWidth';
 import WarningIcon from 'material-ui/svg-icons/alert/warning';
 import {red500} from 'material-ui/styles/colors';
 
+import {getParsedCategories} from 'utils/ebetsCategories';
+
 import NavDrawer from './NavDrawer';
 
 class Master extends Component {
@@ -156,6 +158,36 @@ class Master extends Component {
     this.setState({showUnfeatured: featured});
   }
 
+  tabTitle(path) {
+    if (path === '/new_bet')
+      return 'New bet';
+    if (path === '/faq')
+      return 'F.A.Q.';
+    if (path === '/arbiters')
+      return 'Arbiters';
+    if (path.endsWith('/all_bets'))
+      return 'All bets';
+    if (path.endsWith('/my_bets'))
+      return 'My bets';
+
+    var cats = getParsedCategories();
+    for(var i in cats) {
+      var c = cats[i]
+      if (path.endsWith(c.key))
+        return c;
+    }
+
+    return null;
+  }
+
+  tabTitleDiv(path) {
+    return (
+      <div style={{marginLeft: 12}}>
+        {this.tabTitle(path)}
+      </div>
+    );
+  }
+
   render() {
     const {
       location,
@@ -173,12 +205,14 @@ class Master extends Component {
     const web3Context = this.context.web3;
     var errorMessage = null;
     if (web3Context.networkId !== null && web3Context.networkId !== '42' && web3Context.networkId !== '3') {
-      errorMessage = <div> <WarningIcon color={red500}/> Ebets runs only in the Kovan or Ropsten networks, please switch to use the Dapp</div>;
+      errorMessage = <div style={{display: 'flex', flexFlow: 'row', alignItems: 'center'}}> <WarningIcon color={red500} style={{marginLeft: 12, marginRight: 12}}/>
+        <span>Ebets runs only in the Kovan or Ropsten networks, please switch to use the Dapp</span>
+      </div>;
       //this.props.router.push('');
     }
     else if (web3Context.accounts.length === 0) {
-      errorMessage = <div> <WarningIcon color={red500}/>
-        Make sure you unlock or have at least one account in your wallet
+      errorMessage = <div style={{display: 'flex', flexFlow: 'row', alignItems: 'center'}}> <WarningIcon color={red500} style={{marginLeft: 12, marginRight: 12}}/>
+        <span>Make sure you unlock or have at least one account in your wallet</span>
       </div>;
     }
     return (
@@ -186,7 +220,7 @@ class Master extends Component {
         <div style={{marginLeft: 210}}>
           <AppBar
           onLeftIconButtonTouchTap={this.handleTouchTapLeftIconButton}
-          title={(errorMessage === null ) ? this.props.location.pathname : errorMessage}
+          title={(errorMessage === null ) ? this.tabTitleDiv(this.props.location.pathname) : errorMessage}
           zDepth={0}
           style={styles.appBar}
           showMenuIconButton={false}
